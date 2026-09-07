f237d56e39d92134b4a6e71154a3aa77f3aff6ef
Marjans <kucmaran21@gmail.com>
2026-09-07 09:56:03 +0300

    Ieviesta metode

--- src/git1/Kaulins.java
+++ src/git1/Kaulins.java
@@ -1,9 +1,17 @@
 package git1;
 
+import java.util.Random;
+
 public class Kaulins {
+	static void mestKaulinu() {
+		int skaitlis;
+		Random rand= new Random();
+		skaitlis=rand.nextInt(6)+1;
+		System.out.println("Uzkrita skaitlis: "+skaitlis);
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		mestKaulinu();
 
 	}
 
