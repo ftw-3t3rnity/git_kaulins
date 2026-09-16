fb48f039df8a8d3a5d8dbac3ca83ed0efe1e505e
Marjans <kucmaran21@gmail.com>
2026-09-16 13:52:41 +0300

    Atrod mazako vertibu no visiem meitienem

--- src/git1/Kaulins.java
+++ src/git1/Kaulins.java
@@ -1,17 +1,31 @@
 package git1;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Kaulins {
-	static void mestKaulinu() {
+	static int mestKaulinu(int reizes) {
 		int skaitlis;
 		Random rand= new Random();
+		int min = 0;
+		
+		for(int i=1; i<=reizes;i++){
 		skaitlis=rand.nextInt(6)+1;
 		System.out.println("Uzkrita skaitlis: "+skaitlis);
+		if(skaitlis < min)
+			min=skaitlis;
+		}
+		
+		return min;
 	}
 
 	public static void main(String[] args) {
-		mestKaulinu();
+		int  reizes;
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Cik reizes mest kaulinu?");
+		reizes= scan.nextInt();
+		System.out.println("Mazakais skaitlis no visiem meiteniem: "+mestKaulinu(reizes));
+		
 
 	}
 
